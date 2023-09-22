51d5a7392527a9616d5388a322dc92c924c9c155
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-22 12:27:50 +0530

    sum of first n using function.

---
+++ src/functional.java
@@ -0,0 +1,13 @@
+public class functional {
+    public static void main(String[] args)
+    {
+        int n =3;
+        System.out.println(sum(n));
+    }
+    public static int sum(int n) {
+        if (n == 0)
+            return 0;
+            return n + sum(n - 1);
+
+    }
+}
